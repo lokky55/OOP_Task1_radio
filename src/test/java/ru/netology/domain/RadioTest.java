a103dded112a045f0185745672fd01256a613e70
lokky55 <lokky55@mail.ru>
2022-06-03 17:46:27 +0300

    Common Radio radio = new Radio()

--- src/test/java/ru/netology/domain/RadioTest.java
+++ src/test/java/ru/netology/domain/RadioTest.java
@@ -5,9 +5,10 @@ import org.junit.jupiter.api.Test;
 
 public class RadioTest {
 
+    Radio radio = new Radio();
+
     @Test
     void setNumberOutOfRangeUnderMin() {
-        Radio radio = new Radio();
         radio.setAnyStation(-1);       // пробуем выставить станцию с пульта на -1
         int actual = radio.getCurrentStation();
         int expected = 0;
@@ -16,7 +17,6 @@ public class RadioTest {
 
     @Test
     void setNumberOutOfRangeOverMax() {
-        Radio radio = new Radio();
         radio.setAnyStation(10);       // пробуем выставить станцию с пульта на 10
         int actual = radio.getCurrentStation();
         int expected = 0;
@@ -25,7 +25,6 @@ public class RadioTest {
 
     @Test
     void shouldSetNextStation() {
-        Radio radio = new Radio();
         radio.setCurrentStation(8);     // канал на котором находится радио
         radio.nextStation();            // нажимаем кнопку "channel +"
         int actual = radio.getCurrentStation();
@@ -35,7 +34,6 @@ public class RadioTest {
 
     @Test
     void shouldSetPreviousStation() {
-        Radio radio = new Radio();
         radio.setCurrentStation(1);     // канал на котором находится радио
         radio.previousStation();        // нажимаем кнопку "channel -"
         int actual = radio.getCurrentStation();
@@ -45,7 +43,6 @@ public class RadioTest {
 
     @Test
     void shouldSet0StationAfter9WhenIncrease() {
-        Radio radio = new Radio();
         radio.setCurrentStation(9);     // канал на котором находится радио
         radio.nextStation();            // нажимаем кнопку "channel +"
         int actual = radio.getCurrentStation();
@@ -55,7 +52,6 @@ public class RadioTest {
 
     @Test
     void shouldSet9StationAfter0WhenDecrease() {
-        Radio radio = new Radio();
         radio.setCurrentStation(0);     // канал на котором находится радио
         radio.previousStation();        // нажимаем кнопку "channel -"
         int actual = radio.getCurrentStation();
@@ -65,7 +61,6 @@ public class RadioTest {
 
     @Test
     void shouldIncreaseVolume() {
-        Radio radio = new Radio();
         radio.setCurrentVolume(9);       // актуальная громкость
         radio.increaseVolume();          // увеличиваем громкость
         int actual = radio.getCurrentVolume();
@@ -75,7 +70,6 @@ public class RadioTest {
 
     @Test
     void trySetVolumeOverMax() {
-        Radio radio = new Radio();
         radio.setCurrentVolume(10);   // актуальная громкость
         radio.increaseVolume();       // попробовали выставить громкость выше 10
         int actual = radio.getCurrentVolume();
@@ -85,7 +79,6 @@ public class RadioTest {
 
     @Test
     void shouldDecreaseVolume() {
-        Radio radio = new Radio();
         radio.setCurrentVolume(10);     // актуальная громкость
         radio.decreaseVolume();         // уменьшаем громкость
         int actual = radio.getCurrentVolume();
@@ -95,7 +88,6 @@ public class RadioTest {
 
     @Test
     void trySetVolumeUnderMin() {
-        Radio radio = new Radio();
         radio.setCurrentVolume(0);     // актуальная громкость
         radio.decreaseVolume();        // пробуем уменьшить громкость ниже 0
         int actual = radio.getCurrentVolume();
